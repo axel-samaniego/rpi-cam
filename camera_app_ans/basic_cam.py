b4cc21890a38f2c8fded7284d91a60fcef1d7d75
asamaniego3 <asamaniego3@gatech.edu>
2024-06-04 19:22:38 -0400

    changed app more

--- camera_app_ans/basic_cam.py
+++ camera_app_ans/basic_cam.py
@@ -1,7 +1,8 @@
-import cv2
-import RPi.GPIO as GPIO
 import time
 import os
+import cv2
+import RPi.GPIO as GPIO
+from picamera2 import Picamera2
 
 # Define GPIO pin for the button
 BUTTON_PIN = 21
@@ -11,35 +12,32 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Define the directory to save images
-SAVE_DIR = '~/rpi_axel/images'  # Change this to your desired directory
+SAVE_DIR = os.path.expanduser('~/rpi_axel/images')
+if not os.path.exists(SAVE_DIR):
+    os.makedirs(SAVE_DIR)
 
 
 
-def capture_image(camera):
-    # Capture frame-by-frame
-    ret, frame = camera.read()
-    if not ret:
-        print("Error: Failed to capture image.")
-        return
-
+def capture_image(picam):
     # Define the image filename with timestamp
+    img = picam.capture_array()
     timestamp = time.strftime("%Y%m%d-%H%M%S")
-    image_path = f"{SAVE_DIR}image_{timestamp}.jpg"
+    image_path = os.path.join(SAVE_DIR, f"image_{timestamp}.jpg")
 
-    # Save the captured image
-    cv2.imwrite(image_path, frame)
+    # Capture and save the image
+    cv2.imwrite(image_path, img)
+    
     print(f"Image saved to {image_path}")
 
 def main():
     # Initialize the camera
-    camera = cv2.VideoCapture(1)
-    if not camera.isOpened():
-        print("Error: Could not open camera.")
-        exit()
-    if not os.path.exists(SAVE_DIR):
-        print("Save path does not exist!")
-        exit()
-    
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_still_configuration())
+    picam2.preview_configuration.main.size = (800,800)
+    picam2.preview_configuration.main.format = "RGB888"
+    picam2.preview_configuration.align()
+    picam2.configure("preview")
+    picam2.start()
     try:
         print("Press the button to capture an image...")
         while True:
@@ -47,14 +45,13 @@ def main():
             button_state = GPIO.input(BUTTON_PIN)
             if button_state == GPIO.LOW:
                 print("Button pressed!")
-                capture_image(camera)
+                capture_image(picam2)
                 # Debounce the button press
                 time.sleep(0.5)
     except KeyboardInterrupt:
         print("Program terminated by user.")
     finally:
-        # Release the camera and clean up GPIO
-        camera.release()
+        # Clean up GPIO
         GPIO.cleanup()
         print("Cleanup completed.")
 
