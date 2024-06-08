aaaf33315c535cd9c3be0aa104a848507538bb81
asamaniego3 <asamaniego3@gatech.edu>
2024-06-08 17:31:07 -0400

    changed button number

--- shutdown_reboot/reboot.py
+++ shutdown_reboot/reboot.py
@@ -3,25 +3,24 @@ import time
 import os
 
 # Define the GPIO pin
-BUTTON_PIN = 5
+BUTTON_PIN = 6
+
+# Set up the GPIO pin
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def button_callback(channel):
+    print("Button was pushed!")
+    os.system("sudo reboot")
+
+# Add event detection on the GPIO pin
+GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_callback, bouncetime=300)
 
 try:
-    # Set up the GPIO pin
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-    def button_callback(channel):
-        print("Button was pushed!")
-        os.system("sudo reboot")
-
-    # Add event detection on the GPIO pin
-    GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_callback, bouncetime=300)
-
     while True:
         time.sleep(1)  # Keep the script running
-
+except KeyboardInterrupt:
+    GPIO.cleanup()  # Clean up GPIO on CTRL+C exit
 except Exception as e:
-    print("Error:", e)
-
-finally:
-    GPIO.cleanup()  # Clean up GPIO on script exit
+    print(e)
+    GPIO.cleanup()  # Clean up GPIO on other exceptions
