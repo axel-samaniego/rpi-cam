3f6ee074d7e97c62911e793b960f633c666a8333
asamaniego3 <asamaniego3@gatech.edu>
2024-06-08 18:37:07 -0400

    commit something

--- camera_app_ans/basic_cam_picam.py
+++ camera_app_ans/basic_cam_picam.py
@@ -2,6 +2,7 @@ import time
 import os
 import cv2
 import RPi.GPIO as GPIO
+from picamera2 import Picamera2
 
 # Define GPIO pin for the button
 BUTTON_PIN = 21
@@ -17,25 +18,26 @@ if not os.path.exists(SAVE_DIR):
 
 
 
-def capture_image(cam):
+def capture_image(picam):
     # Define the image filename with timestamp
-    # Capture frame-by-frame
-    ret, frame = cam.read()
-    if not ret:
-        print("Error: Failed to capture image.")
-        return
-
+    img = picam.capture_array()
     timestamp = time.strftime("%Y%m%d-%H%M%S")
     image_path = os.path.join(SAVE_DIR, f"image_{timestamp}.jpg")
 
     # Capture and save the image
-    cv2.imwrite(image_path, frame)
+    cv2.imwrite(image_path, img)
+    
     print(f"Image saved to {image_path}")
 
 def main():
     # Initialize the camera
-    cam = cv2.VideoCapture(0, cv2.CAP_V4L2)
-    
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_still_configuration())
+    picam2.preview_configuration.main.size = (800,800)
+    picam2.preview_configuration.main.format = "RGB888"
+    picam2.preview_configuration.align()
+    picam2.configure("preview")
+    picam2.start()
     try:
         print("Press the button to capture an image...")
         while True:
@@ -43,7 +45,7 @@ def main():
             button_state = GPIO.input(BUTTON_PIN)
             if button_state == GPIO.LOW:
                 print("Button pressed!")
-                capture_image(cam)
+                capture_image(picam2)
                 # Debounce the button press
                 time.sleep(0.5)
     except KeyboardInterrupt:
