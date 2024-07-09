49a1f977fe45e674a9084ddd269c1711f8233529
asamaniego3 <asamaniego3@gatech.edu>
2024-07-08 23:51:12 -0400

    trying to fix button code

--- camera_app_ans/start_on_button.py
+++ camera_app_ans/start_on_button.py
@@ -16,16 +16,12 @@ try:
         input_state = GPIO.input(GPIO_PIN)
         if input_state == GPIO.LOW:
             print("Button pressed!")
-            print("Running Program!")
-            # Replace '/path/to/your_program' with the actual path to your program
-            
-            time.sleep(0.5)  # Debounce time
-            while GPIO.input(GPIO_PIN) == GPIO.LOW:
-                time.sleep(0.01)  # Wait for button release
+            print("Running Program!")            
             break  # Exit the loop and terminate the script
 
-        time.sleep(0.01)  # Polling interval
+        time.sleep(0.1)  # Polling interval
 
 finally:
     GPIO.cleanup()  # Clean up GPIO on exit
     subprocess.Popen(['python', '/home/rpi_axel/rpi_axel/projects/rpi-cam/camera_app_ans/run_picam.py'])
+    exit()
