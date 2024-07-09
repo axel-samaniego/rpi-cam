af7c2f1c93bbfce047fd3da3371df3eda2d3c18f
asamaniego3 <asamaniego3@gatech.edu>
2024-07-08 23:25:39 -0400

    trying to add pi

--- camera_app_ans/button_script.sh
+++ camera_app_ans/button_script.sh
@@ -6,13 +6,13 @@ GPIO=6
 # Loop indefinitely
 while true; do
     # Check if button is pressed
-    if [ "$(gpio read $GPIO)" = "0" ]; then
+    if [ "$(pigpio read $GPIO)" = "0" ]; then
         echo "Button pressed!"
         echo "Starting Program!"
         # Replace '/path/to/your_program' with the actual path to your program
         python run_picam.py &
         sleep 0.5  # Debounce time
-        while [ "$(gpio read $GPIO)" = "0" ]; do
+        while [ "$(pigpio read $GPIO)" = "0" ]; do
             sleep 0.1  # Wait for button release
         done
         echo "Button released!"
